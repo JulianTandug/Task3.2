8b49495dfc9a1e26a5a6eedfbbf728cf3203f8a2
Julian The Tandug <159874831+JulianTandug@users.noreply.github.com>
2025-12-11 14:34:47 +0800

    Update mp1script.js

--- mp1script.js
+++ mp1script.js
@@ -1,11 +1,10 @@
 function replaceAll() {
     let str1 = document.getElementById("txtInput").value;
     console.log(str1);
-if (str1.includes('@'))  {
-    document.getElementByid("txtResult").textContent = "valid";
-} else {
-    document.getElementByid("txtResult").textContent = "invalid";
 
+    if (str1.includes('@')) {
+        document.getElementById("txtResult").textContent = "valid";
+    } else {
+        document.getElementById("txtResult").textContent = "invalid";
+    }
 }
-
-)};
